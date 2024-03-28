2a6315c73a1efe664e483a6c645c596a75b911ed
wuhaz <164835892+wuhaz@users.noreply.github.com>
2024-03-27 18:38:46 -0800

    Create Revenant UI Lib.lua

---
+++ Revenant UI Lib.lua
@@ -0,0 +1,6 @@
+# Boot Library 
+```lua
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Revenant", true))()
+local Flags = Library.Flags
+```
+--Library.DefaultColor = Color3.fromRGB(65, 143, 232)
